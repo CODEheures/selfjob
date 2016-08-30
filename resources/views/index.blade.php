bfe67ed3f594f1b281a25c4f55cac2e1942cc6b7
Sylvain <webmaster@ecortex.fr>
2016-08-30 12:41:11 +0200

    ajoute progress-bar-striped active à la bar de progression

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -121,7 +121,7 @@
                             <div class="row">
                                 <div class="col-xs-12">
                                     <div class="progress">
-                                        <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 82%;">
+                                        <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 82%;">
                                             82%
                                         </div>
                                     </div>
